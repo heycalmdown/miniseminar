e598b0482821f79cbfb5a19f5b7c25c4b318b459
heycalmdown <heyacct@gmail.com>
2016-02-14 18:58:11 +0900

    add) support to grab a content from a confluence

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import Confluency from 'confluency';
+
 const router = express.Router();
+const confluency = new Confluency({host: 'https://confluency.atlassian.net', context: 'wiki'}); 
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -7,8 +10,11 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/page/:id', (req, res, next) => {
-  console.log(req.params);
-  res.render('page');
+  // storage
+  // editor
+  confluency.getPage(req.params.id, ['body.storage']).then(page => {
+    res.render('page', { title: page.title, contents: page.body.storage.value });
+  }).catch(next);
 });
 
 module.exports = router;
