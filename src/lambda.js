19276c323651a583b8b9932c46ca0920bee4ee73
heycalmdown <heyacct@gmail.com>
2016-09-12 21:58:58 +0900

    change) lambda.js cleanup

--- src/lambda.js
+++ src/lambda.js
@@ -1,116 +1,80 @@
-var http = require('http')
+const _ = require('lodash');
 
-/*
- * Split one of API Gateway's param strings into a real javascript object
- * @param {string} paramString Looks like {key1=val1, key2=val2}
- */
-function parseParamString(paramString) {
-  var obj = {};
+function parseParams(params) {
+  if (!params) return {};
 
-  if (typeof paramString !== "undefined") {
-    paramString
-      .substring(1, paramString.length - 1) //strip off { and }
-      .split(", ")
-      .forEach(function (keyVal) {
-        var pieces = keyVal.split("=");
-        var key = pieces[0],
-          val = pieces[1];
+  const objs = params.slice(1, -1).split(', ')
+    .map(kv => kv.split('='))
+    .map(([k, v]) => {
+      if (v === 'true') v = true;
+      if (v === 'false') v = false;
+      return {[k]: v};
+    });
+  return _.merge({}, ...objs);
+}
 
-        //Force "true" and "false" into Boolean
-        if (val === "true") val = true;
-        if (val === "false") val = false;
+function convertUrl(path, req) {
+  const pathname = Object.keys(req.pathParams).reduce((path, p) => {
+    return path.replace(`{${p}}`, req.pathParams[p]);
+  }, path);
+  const query = req.queryParams;
 
-        obj[key] = val;
-      });
-  }
-
-  return obj;
-};
-
-
-/*
- * Generate a somewhat normal path
- */
-function reconstructUrl(path, request) {
-  //Append query string
-  if (Object.keys(request.queryParams).length > 0) {
-    var str = [];
-    for (var p in request.queryParams) {
-      if (request.queryParams.hasOwnProperty(p) && p != '') {
-        str.push(p + "=" + request.queryParams[p]);
-      }
-    }
-    if (str.length > 0) {
-      path += "?" + str.join("&");
-    }
-  }
-
-  //Fix path parameters
-  if (Object.keys(request.pathParams).length > 0) {
-    for (var param in request.pathParams) {
-      if (request.pathParams.hasOwnProperty(param)) {
-        var toReplace = "{" + param + "}";
-        path = path.replace(toReplace, request.pathParams[param]);
-      }
-    }
-  }
-  return path;
+  const url = require('url');
+  return url.format({pathname, query});
 }
 
 function mapEvent(event) {
-  var request = {};
-  request.queryParams = {};
+  const req = {};
+  req.queryParams = {};
 
-  if (typeof event.queryString !== "undefined") {
-    request.queryParams = parseParamString(event.queryString);
+  if (event.queryString) {
+    req.queryParams = parseParams(event.queryString);
   }
 
-  if (typeof event.headers !== "undefined") {
-    request.headers = parseParamString(event.headers);
-    request.headers["user-agent"] = event["user-agent"];
+  if (event.headers) {
+    req.headers = parseParams(event.headers);
+    req.headers["user-agent"] = event["user-agent"];
 
-    request.headers["x-real-ip"] = event["source-ip"];
-    request.headers["host"] = event["api-id"]
+    req.headers["x-real-ip"] = event["source-ip"];
+    req.headers["host"] = event["api-id"]
   }
-  request.pathParams = parseParamString(event.pathParams);
+  req.pathParams = parseParams(event.pathParams);
 
-  request.method = event["http-method"];
-  request.url = reconstructUrl(event['resource-path'], request);
-  request.baseUrl = '';
-  request.originalUrl = request.baseUrl + request.url;
+  req.method = event["http-method"];
+  req.url = convertUrl(event['resource-path'], req);
+  req.baseUrl = '';
+  req.originalUrl = req.baseUrl + req.url;
   if (event.baseUrl) {
-    request.baseUrl = event.baseUrl;
+    req.baseUrl = event.baseUrl;
   }
 
-  delete request.allParams;
-  delete request.queryString;
-
-  var fake_sock = {
+  const socket = {
     remoteAddress: event.remoteAddress
   };
 
-  request.socket = fake_sock;
-  request.connection = fake_sock;
+  req.socket = socket;
+  req.connection = socket;
 
-  return request;
+  return req;
 }
 
 const app = require('./app');
 exports.handler = function (event, context) {
-  var req = mapEvent(event);
-  var res = new http.ServerResponse(req);
+  const http = require('http')
+  const req = mapEvent(event);
+  const res = new http.ServerResponse(req);
 
-  res.original_end = res.end;
+  res.oldEnd = res.end;
   res.end = function (chunk, encoding, callback) {
-    res.original_end(chunk, encoding, callback);
-    var statusCode = res.statusCode;
+    res.oldEnd(chunk, encoding, callback);
+    const statusCode = res.statusCode;
 
     if (statusCode > 399) {
       return context.fail(new Error(statusCode));
     }
 
-    var contentType = res.getHeader('content-type');
-    var payload = res.output[1].toString('base64');
+    const contentType = res.getHeader('content-type');
+    const payload = res.output[1].toString('base64');
     context.succeed({ payload, contentType });
   };
 
