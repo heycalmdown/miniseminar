1c7a20fc4608e77084d3b2a8ee0bdbe383ab209b
heycalmdown <heyacct@gmail.com>
2018-11-28 16:11:16 +0900

    refactor: bump up reveal.js

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../node_modules/reveal.js')));
 
 app.use('/', routes);
 app.use('/users', users);
